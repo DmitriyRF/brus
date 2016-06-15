1c3fd44b14afbb3265b71e16c22a3b276f004f94
DmitriyRF <dmitriy_r_f@mail.ru>
2016-06-15 12:16:17 +0300

    for() for event

--- js/common.js
+++ js/common.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
+	//Для заполнения всего начального экрана
+	function WindowResize(){
+		$(".js-resize").css("height",$(window).height());
+	}
+	WindowResize();
+	$(window).resize(function () {
+		WindowResize();
+	})
+
+	//Для табов
+	$(".tab_item").not(":first").hide();
+	var ElementsW =document.body.getElementsByClassName("js-wrapper");
+	var quantityW = ElementsW.length;
+	if(quantityW){
+		for(var i = 0; i<quantityW; i++){
+			ElementsW[i].addEventListener('click',function(event){
+				var target = event.target; // где был клик?
+				if(target.tagName === "span"){
+					this.getElementsByClassName(tab).removeClass("active").eq($(this).index()).addClass("active");
+				}
+			},false);
+		}
+	}
+
+	//$(".js-wrapper .tab").click(function() {
+	//	$(this).removeClass("active").eq($(this).index()).addClass("active");
+	//	var TabEl = this.parentNode.parentNode.getElementsByClassName('tab');
+	//	/*this.parentNode.hide().eq($(this).index()).fadeIn()*/
+	//}).eq(0).addClass("active");
+	/*
+	$(".tab_item").not(":first").hide();
+	$(".js-wrapper .tab").click(function() {
+		$(".js-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+		$(".tab_item").hide().eq($(this).index()).fadeIn()
+	}).eq(0).addClass("active");*/
+
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
