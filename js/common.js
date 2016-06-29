58be1e429375ed9386518b11bc5b127104560a95
DmitriyRF <dmitriy_r_f@mail.ru>
2016-06-29 23:01:40 +0300

    new section detailed description

--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
-//Для заполнения всего начального экрана ----------------+----------------
+
+//Для заполнения всего начального экрана ------------+-------------------------+-------------------------+-------------
+
     function WindowResize() {
         $(".js-resize").css("min-height", $(window).height());
         $(".fon-white").css("min-height", $(window).height());
@@ -10,7 +12,8 @@ jQuery(document).ready(function ($) {
         WindowResize();
     });
 
-    //Для табов---------------------+-------------------------+--------------
+//Для табов------------+-------------------------+-------------------------+-------------------------+-------------
+
     $(".tel .tab_item").not(":first").hide();
     $(".tel .js-wrapper .tab").click(function() {
         $(".tel .js-wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
@@ -29,7 +32,28 @@ jQuery(document).ready(function ($) {
         $(".js-project .tab_item").hide().eq($(this).index()).fadeIn()
     }).eq(0).addClass("active");
 
-    //Цели для Яндекс.Метрики и Google Analytics ------------+-------------
+// FANSYBOX  ------------+-------------------------+-------------------------+-------------------------+-------------
+//http://fancyapps.com/
+    ("a.gallery").fancybox({                       
+            // "padding" : 20,//padding - отступ контента (фотографий) от краев окна.
+            // "imageScale" : false,   //imageScale - true - контент масштабируется по размеру окна, 
+            //                         // false - окно вытягивается по размеру контента. По умолчанию - true;
+            // "zoomOpacity" : false,  //zoomOpacity - изменение прозрачности контента во время анимации
+            // "zoomSpeedIn" : 1000,   //zoomSpeedIn - скорость анимации в мс при увеличении фото (по умолчанию 0);
+            // "zoomSpeedOut" : 1000,  //zoomSpeedOut - скорость анимации в мс при уменьшении фото (по умолчанию 0) ;
+            // "zoomSpeedChange" : 1000, //zoomSpeedChange - скорость анимации в мс при смене фото (по умолчанию 0);
+            // "frameWidth" : 700,  //frameWidth - ширина окна, px (425px - по умолчанию);
+            // "frameHeight" : 600, //frameHeight - высота окна, px(355px - по умолчанию);
+            // "overlayShow" : true, //overlayShow - (по умолчанию true) если true, то затенят страницу под всплывающим окном. 
+            //                         //Цвет задается в jquery.fancybox.css - div#fancy_overlay 
+            // "overlayOpacity" : 0.8, //overlayOpacity - Прозрачность затемнения (0.3 по умолчанию);
+            // "hideOnContentClick" :false, //hideOnContentClick -  true закрывает окно по клику по любой его точке (кроме элементов навигации). По умолчанию true;
+            // "centerOnScroll" : false //centerOnScroll - Если true, то окно центрируется на экране, когда пользователь прокручивает страницу.
+                
+            });
+
+//Цели для Яндекс.Метрики и Google Analytics ------------+-------------------------+-------------------------+-------------
+
     $(".count_element").on("click", (function () {
         ga("send", "event", "goal", "goal");
         yaCounterXXXXXXXX.reachGoal("goal");
@@ -43,8 +67,9 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    //Аякс отправка форм
-    //Документация: http://api.jquery.com/jquery.ajax/
+//Аякс отправка форм ------------+-------------------------+-------------------------+-------------------------+-------------
+//Документация: http://api.jquery.com/jquery.ajax/
+  
     $("#form").submit(function (e) {
         e.preventDefault;
         $.ajax({
